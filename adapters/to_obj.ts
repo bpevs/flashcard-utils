48d49c31c21850222f0b0a24ca0c13064340c936
Ben Pevsner <git@bpev.me>
2024-02-16 17:10:18 -0600

    chore: remove unnecessary field order

--- adapters/to_obj.ts
+++ adapters/to_obj.ts
@@ -2,14 +2,8 @@ import type { ExportObj } from '../models/types.ts'
 import Deck from '../models/deck.ts'
 import Note from '../models/note.ts'
 
-export default function toObj(
-  deck: Deck,
-  { fieldOrder }: { fieldOrder?: string[] } = {},
-): ExportObj {
-  if (fieldOrder && (fieldOrder.length !== deck.content.fields.length)) {
-    throw new Error('fieldOrder has the incorrect number of fields!')
-  }
-  const fields = fieldOrder || deck.content.fields
+export default function toObj(deck: Deck): ExportObj {
+  const fields = deck.content.fields
 
   const data: ExportObj = {
     id: deck.id,
